0e16f4ab91f649ba5b241016dbe4fb9d119524b0
martinaperriera <168413277+martinaperriera@users.noreply.github.com>
2024-11-18 15:43:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sostituisci la porta nel file di configurazione di Apache
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/:80/:${PORT}/g" /etc/apache2/sites-available/000-default.conf
+sed -i "s/Listen 80/Listen ${1000}/" /etc/apache2/ports.conf
+sed -i "s/:80/:${1000}/g" /etc/apache2/sites-available/000-default.conf
 
 # Avvia Apache in foreground
 apache2-foreground
